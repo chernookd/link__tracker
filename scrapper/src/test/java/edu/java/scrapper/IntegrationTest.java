864828890c6c2ca28a1ef902834a47ad2227b856
dcher <cernookijdima7@gmail.com>
2024-03-10 21:27:19 +0300

    hw4

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,10 +1,19 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.SearchPathResourceAccessor;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.sql.SQLException;
 
 @Testcontainers
 public abstract class IntegrationTest {
@@ -21,7 +30,25 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
+        dataSource.setSuppressClose(true);
+        dataSource.setUrl(c.getJdbcUrl());
+        dataSource.setUsername(c.getUsername());
+        dataSource.setPassword(c.getPassword());
+
+        String path = new File(".").toPath().toAbsolutePath().getParent().getParent().resolve("migrations").toString();
+
+        try {
+            Liquibase liquibase = new Liquibase(
+                "master.xml",
+                new SearchPathResourceAccessor(path),
+                new JdbcConnection(dataSource.getConnection())
+            );
+
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (LiquibaseException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
